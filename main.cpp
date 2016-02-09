8bfa6f386cafd4ed67525349f395a205e48d5119
Andreas Braun <andreasbraun5@aol.com>
2016-02-09 09:57:48 +0100

    initial2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <QCoreApplication>
-
+#include <QDebug>
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    qDebug() << "hello world";
+
+    //std::cout<<"Hello World!\n";
 
     return a.exec();
 }
